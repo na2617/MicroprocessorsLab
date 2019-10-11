9e4ceaf86881a6de9dc67c8c45fff79165eb2260
na2617 <na2617@skbl-422-007.ph.ic.ac.uk>
2019-10-11 10:29:03 +0100

    introduced delay

--- Simple1.asm
+++ Simple1.asm
@@ -7,14 +7,20 @@
 	org 0x100		    ; Main code starts here at address 0x100
 
 start
+	movlw	0x3
+	movwf	0x400
 	movlw 	0x0
 	movwf	TRISC, ACCESS	    ; Port C all outputs
 	bra 	test
+delay	decfsz	0x400
+	bra	delay
+	return
 loop	movff 	0x06, PORTC
 	incf 	0x06, W, ACCESS
 test	movwf	0x06, ACCESS	    ; Test for end of loop condition
 	movf 	PORTD, W, ACCESS
 	cpfsgt 	0x06, ACCESS
+	bra	delay
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
 	
