cee6854f7357765d7263dbe2de5430f4781cf1df
na2617 <na2617@skbl-422-007.ph.ic.ac.uk>
2019-10-14 16:29:57 +0100

    2 byte write function written

--- Simple1.asm
+++ Simple1.asm
@@ -9,26 +9,30 @@
 	; ******* Programme FLASH read Setup Code ****  
 setup	bcf	EECON1, CFGS	; point to Flash program memory  
 	bsf	EECON1, EEPGD 	; access Flash program memory
+	clrf TRISD
+	movlw 0x03
+	movwf LATD
+	clrf TRISE
 	goto	start
-	; ******* My data and where to put it in RAM *
+
+; ******* My data and where to put it in RAM *
 myTable data	"This is just some data"
 	constant 	myArray=0x400	; Address in RAM for data
 	constant 	counter=0x10	; Address of counter variable
 	; ******* Main programme *********************
-start 	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
-	movlw	upper(myTable)	; address of data in PM
-	movwf	TBLPTRU		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL		; load low byte to TBLPTRL
-	movlw	.22		; 22 bytes to read
-	movwf 	counter		; our counter register
-loop 	tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0	; move read data from TABLAT to (FSR0), increment FSR0	
-	decfsz	counter		; count down to zero
-	bra	loop		; keep going until finished
 	
-	goto	0
-
+start	movlw 0x0F	    ;data trying to store
+	call write
+	goto start
+	
+write	movwf LATE
+	clrf TRISE
+	movlw 0x01
+	movwf LATD
+	nop
+	nop
+	movlw 0x03
+	movwf LATD
+	setf TRISE
+	return
 	end
