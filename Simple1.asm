d55b38cab6b02eca7aff5d81560554b878ec96c1
na2617 <na2617@skbl-422-007.ph.ic.ac.uk>
2019-10-18 09:24:39 +0100

    serial to parallel data transfer

--- Simple1.asm
+++ Simple1.asm
@@ -2,52 +2,34 @@
 	
 	code
 	org 0x0
-	goto	setup
+	goto	start
 	
 	org 0x100		    ; Main code starts here at address 0x100
 
-	; ******* Programme FLASH read Setup Code ****  
-setup	bcf	EECON1, CFGS	; point to Flash program memory  
-	bsf	EECON1, EEPGD 	; access Flash program memory
-	clrf	TRISD
-	movlw	0x03
-	movwf	LATD
-	clrf	TRISE
-	goto	start
-
-; ******* My data and where to put it in RAM *
-myTable data	"This is just some data"
-	constant 	myArray=0x400	; Address in RAM for data
-	constant 	counter=0x10	; Address of counter variable
-	; ******* Main programme *********************
-	
 start	
-	banksel PADCFG1 ; PADCFG1 is not in Access Bank!!
-	bsf	PADCFG1, REPU, BANKED ; PortE pull-ups on
-	movlb	0x00 ; set BSR back to Bank 0
-	setf	TRISE ; Tri-state PortE
+	call SPI_MasterInit
+	movlw 0x05
+	call SPI_MasterTransmit
+    
+SPI_MasterInit ; Set Clock edge to negative
 	
-	movlw	0x0F	    ;data trying to store
-	call	write
-	call	read
-	goto	start
-	
-write	movwf	LATE
-	clrf	TRISE
-	movlw	0x01
-	movwf	LATD
-	nop
-	nop
-	movlw	0x03
-	movwf	LATD
-	setf	TRISE
+	bcf SSP2STAT, CKE
+	; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
+	movlw (1<<SSPEN)|(1<<CKP)|(0x02)
+	movwf SSP2CON1
+	; SDO2 output; SCK2 output
+	bcf TRISD, SDO2
+	bcf TRISD, SCK2
 	return
-	
-	
-read	movlw	0x2
-	movwf	LATD
-	clrf	TRISC
-	movff	PORTE, PORTC
+    
+SPI_MasterTransmit ; Start transmission of data (held in W)
+	movwf SSP2BUF
+    
+Wait_Transmit ; Wait for transmission to complete
+	btfss PIR2, SSP2IF
+	bra Wait_Transmit
+	bcf PIR2, SSP2IF ; clear interrupt flag
 	return
-	
+    
 	end
+    
